21a0b31a7c1ac26ed23895c523433344d71f7d6f
BetPom <b.pomalecka8@gmail.com>
2022-10-03 22:49:18 +0200

    Java lessons

---
+++ src/Lesson1.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Lesson1 {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        BigInteger years;
+        System.out.print("Podaj swoje imię: ");
+        name = scanner.nextLine();
+        System.out.println("Hello " + name);
+        System.out.print("Ile masz lat? ");
+        years = scanner.nextBigInteger();
+        System.out.printf("Masz %s lat", years);
+
+    }
+}
