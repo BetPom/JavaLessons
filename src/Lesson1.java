54086e1afb17e936eadbd5958f1ebace7e41f5e9
BetPom <b.pomalecka8@gmail.com>
2022-10-12 20:10:04 +0200

    if

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -1,19 +1,25 @@
 import java.math.BigInteger;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Lesson1 {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
         String name;
-        BigInteger years;
+        Integer years;
         System.out.print("Podaj swoje imię: ");
         name = scanner.nextLine();
         System.out.println("Hello " + name);
         System.out.print("Ile masz lat? ");
-        years = scanner.nextBigInteger();
+        years = scanner.nextInt();
         System.out.printf("Masz %s lat", years);
-        years = scanner.nextBigInteger();
-        System.out.printf("Masz %s lat", years);
+        if (years >= 18) {
+            System.out.println("\nMożesz kupić piwo");
+        }
+        else {
+            System.out.println("\nNie możesz kupić piwa");
+        }
+
 
     }
 }
